2b01a38ed9d0402484377c2402c0568164de9021
myoder345 <myoder345@gmail.com>
2017-10-27 19:23:27 -0400

    Camera now slightly leads player's movement. Player has had tank controls added for testing purposes (please don't actually use them, they're marked PLACEHOLDER for a reason).
    Future updates will attempt to smooth camera movement so it's not
    as much of a "SNAP!" motion. This feature will be disableable via
    a boolean in case the character movement curve already does this.

--- Assets/Scripts/CameraTrack.cs
+++ Assets/Scripts/CameraTrack.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class CameraTrack : MonoBehaviour {
 
     public GameObject target;
+    public float lead;
+    Rigidbody targetBod;
 
 	// Use this for initialization
 	void Start () {
-		
+        targetBod = target.GetComponent<Rigidbody>();
+        print(targetBod != null);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-        transform.LookAt(target.transform, Vector3.up);
+        // transform.LookAt(target.transform.position + (targetBod != null ? targetBod.velocity * lead : Vector3.zero), Vector3.up);
+        transform.rotation = Quaternion.LookRotation((targetBod != null ? (target.transform.position + targetBod.velocity * lead) : target.transform.position) - transform.position, Vector3.up);
+        print(targetBod.velocity);
 	}
 }
---
+++ Assets/Scripts/TankControlsPLACEHOLDER.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TankControlsPLACEHOLDER : MonoBehaviour {
+
+	public float speed;
+	public float turnSpeed;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("w"))
+			// transform.Translate(0, 0, speed * Time.deltaTime);
+			rb.velocity = new Vector3(speed * Mathf.Sin(transform.eulerAngles.y * Mathf.Deg2Rad), 0, speed * Mathf.Cos(transform.eulerAngles.y * Mathf.Deg2Rad));
+		else if(Input.GetKey("s"))
+			// transform.Translate(0, 0, -0.5f * speed * Time.deltaTime);
+			rb.velocity = new Vector3(-0.5f * speed * Mathf.Sin(transform.eulerAngles.y * Mathf.Deg2Rad), 0, -0.5f * speed * Mathf.Cos(transform.eulerAngles.y * Mathf.Deg2Rad));
+		else
+			rb.velocity = Vector3.zero;
+		if(Input.GetKey("a"))
+			transform.Rotate(0, -1 * turnSpeed * Time.deltaTime, 0);
+		if(Input.GetKey("d"))
+			transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+	}
+}
