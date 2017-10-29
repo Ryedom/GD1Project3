c91479155d65af3996be00a0efb19439833ee8ca
myoder345 <myoder345@gmail.com>
2017-10-29 18:29:35 -0400

    Camera now eases into leading the character. (Easing out TODO) Camera lead distance is now consistent regardless of character distance to camera.

--- Assets/Scripts/CameraTrack.cs
+++ Assets/Scripts/CameraTrack.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class CameraTrack : MonoBehaviour {
 
     public GameObject target;
-    public float lead;
+    AnimationCurve ease;
+    public float accelTime;
+    float startTime;
+    public float maxLead;
+    float lead;
+    float radius;
     Rigidbody targetBod;
+    bool starting;
 
 	// Use this for initialization
 	void Start () {
         targetBod = target.GetComponent<Rigidbody>();
         print(targetBod != null);
+        starting = false;
+        lead = 0;
+        ease = AnimationCurve.EaseInOut(0f, 0f, accelTime, maxLead);
 	}
 
 	// Update is called once per frame
 	void Update () {
+        radius = Vector3.Distance(transform.position, target.transform.position);
         // transform.LookAt(target.transform.position + (targetBod != null ? targetBod.velocity * lead : Vector3.zero), Vector3.up);
+        // I'm sorry for nesting a ternary expressoin, it made sense at the time
+        if(Input.GetKeyDown("w") || Input.GetKeyDown("s")) {
+            starting = true;
+            startTime = Time.time;
+        }
+
+        lead = ease.Evaluate(Time.time - startTime) * (radius / 100);
         transform.rotation = Quaternion.LookRotation((targetBod != null ? (target.transform.position + targetBod.velocity * lead) : target.transform.position) - transform.position, Vector3.up);
-        print(targetBod.velocity);
+        // print(targetBod.velocity);
 	}
 }
