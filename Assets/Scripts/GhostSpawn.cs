e33488212c92b3eb88762c3149bc81533c0fc8e4
Claire Thomas <clairethomas711@gmail.com>
2017-11-05 17:55:02 -0500

    Added Ghost spawning script

---
+++ Assets/Scripts/GhostSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSpawn : MonoBehaviour {
+
+	public GameObject ghost;
+	public float spawnrate = 1;
+	public float radius = 100;
+	public int spawnpoint_count = 10;
+	List<Vector3> spawnpoints = new List<Vector3>();
+
+
+	// Use this for initialization
+	void Start () {
+		Vector3 center = transform.position;
+		float angle = (360 / spawnpoint_count) * Mathf.Deg2Rad;
+
+		for(int i = 1; i <= spawnpoint_count; i++){
+			float x = radius * Mathf.Cos(angle*i) + center.x;
+			float y = center.y;
+			float z = radius * Mathf.Sin(angle*i) + center.z;
+			Vector3 temp = new Vector3 (x, y, z);
+			spawnpoints.Add(temp);
+		}
+		print ("Calling Invoke");
+		InvokeRepeating ("Spawn", spawnrate, spawnrate);
+	}
+	
+	// Update is called once spawnrate
+	void Spawn () {
+
+		int i = Random.Range (0, spawnpoint_count);
+		Vector3 ghost_pos = spawnpoints [i];
+		Quaternion ghost_rotation = Quaternion.LookRotation(transform.position- ghost_pos);
+		Instantiate (ghost, ghost_pos, ghost_rotation);
+	}
+}
